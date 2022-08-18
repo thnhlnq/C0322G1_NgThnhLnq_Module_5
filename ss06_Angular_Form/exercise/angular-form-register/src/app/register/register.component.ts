371c6639dd2e749d5d59d80a5b9539dff696f24a
thnhlnq <ntlonq14@gmail.com>
2022-08-18 12:20:29 +0700

    ss

---
+++ ss06_Angular_Form/exercise/angular-form-register/src/app/register/register.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() {
+  }
+
+  registerForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    country: new FormControl('', [Validators.required]),
+    age: new FormControl('', [Validators.required, Validators.min(18)]),
+    gender: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.required, Validators.pattern('^\\+84\\d{9,10}$')])
+  }, this.checkPassword);
+
+  ngOnInit(): void {
+  }
+
+  submit() {
+    alert('Sign Up Success..');
+  }
+
+  checkPassword(confirm: AbstractControl) {
+    const value1 = confirm.value.password;
+    const value2 = confirm.value.confirmPassword;
+    if (value1 !== value2) {
+      return {check: true};
+    }
+    return null;
+  }
+}
