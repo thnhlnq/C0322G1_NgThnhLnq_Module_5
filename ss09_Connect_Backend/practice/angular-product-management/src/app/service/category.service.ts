82edf0dc0117e5993d981234f34affabaf246fdd
thnhlnq <ntlonq14@gmail.com>
2022-08-21 23:14:14 +0700

    ss

---
+++ ss09_Connect_Backend/practice/angular-product-management/src/app/service/category.service.ts
@@ -0,0 +1,38 @@
+import {Injectable} from '@angular/core';
+import {Category} from '../model/category';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs';
+
+const API_URL = `${environment.apiUrl}`;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  categories: Category[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAll(): Observable<Category[]> {
+    return this.http.get<Category[]>(API_URL + '/categories');
+  }
+
+  saveCategory(category): Observable<Category> {
+    return this.http.post<Category>(API_URL + '/categories', category);
+  }
+
+  findById(id: number): Observable<Category> {
+    return this.http.get<Category>(`${API_URL}/categories/${id}`);
+  }
+
+  updateCategory(id: number, category: Category): Observable<Category> {
+    return this.http.put<Category>(`${API_URL}/categories/${id}`, category);
+  }
+
+  deleteCategory(id: number): Observable<Category> {
+    return this.http.delete<Category>(`${API_URL}/categories/${id}`);
+  }
+}
